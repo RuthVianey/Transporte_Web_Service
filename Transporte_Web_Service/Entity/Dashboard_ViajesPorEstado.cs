97bf14ced83e97f22117b0410963c48619a35bf2
omarnahu2026 <omarnahu@hotmail.com>
2026-05-26 12:08:39 -0600

    1

--- Transporte_Web_Service/Entity/Dashboard_ViajesPorEstado.cs
+++ Transporte_Web_Service/Entity/Dashboard_ViajesPorEstado.cs
@@ -17,6 +17,25 @@
         public decimal UtilidadTotal { get; set; }
     }
 
+    public class Dasboard_ResumenOperativo
+    {
+        public int TotalViajes { get; set; }
+        public int ViajesPendientes { get; set; }
+        public int ViajesEnCurso { get; set; }
+        public int ViajesCerrados { get; set; }
+        public decimal IngresoTotal { get; set; }
+        public decimal GastosDirectos { get; set; }
+        public decimal Combustible { get; set; }
+        public decimal Mantenimiento { get; set; }
+        public decimal CostoTotal { get; set; }
+        public decimal UtilidadTotal { get; set; }
+        public decimal MargenPorcentaje { get; set; }
+        public decimal KmTotales { get; set; }
+        public decimal CostoPorKm { get; set; }
+        public decimal LitrosCombustible { get; set; }
+        public decimal RendimientoKmPorLitro { get; set; }
+}
+
     public class Dashboard_ViajesPorEstado
     {
         public int IdEstadoViaje { get; set; }
